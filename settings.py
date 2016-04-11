9896fc4d99870dd091cb72278822fe61a06a9b61
Manthan Vaghasiya <mvaghasiya1412@gmail.com>
2016-04-11 22:57:10 +0530

    Postgres link updated

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 api = Api(app)
 # api = Api(app, catch_all_404s=True)
 
-engine = create_engine('postgresql://postgres:ams@localhost:5432/ams')
-# engine = create_engine('postgres://qogoqipkzqtylc:VZNm-dB2SiYTlc7uezU7NbW_fJ@ec2-54-163-254-231.compute-1.amazonaws.com:5432/d164pdjkve1t6d')
+# engine = create_engine('postgresql://postgres:ams@localhost:5432/ams')
+engine = create_engine('postgres://qogoqipkzqtylc:VZNm-dB2SiYTlc7uezU7NbW_fJ@ec2-54-163-254-231.compute-1.amazonaws.com:5432/d164pdjkve1t6d')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
